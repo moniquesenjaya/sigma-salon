aaac441df9cf871ffe0fd245c6ff41ca5cb5af41
Jason J. W <71363607+digaji@users.noreply.github.com>
2022-01-18 21:01:31 +0700

    Implemented admin/all_tables

---
+++ src/backend/api/tableapi.py
@@ -0,0 +1,43 @@
+from src.backend.db import get_db, get_error
+
+
+def get_all_tables():
+    results = []
+
+    db = get_db()
+    cursor = db.cursor()
+
+    try:
+        # Query all the tables and append the result in to results list
+        # person table
+        cursor.execute("SELECT * FROM person;")
+        results.append(cursor.fetchall())
+
+        # customer data
+        cursor.execute("SELECT p.personId, p.username, p.firstName, p.lastName, p.sex, p.dateOfBirth, c.membership FROM person AS p JOIN customers AS c ON c.personId = p.personId;")
+        results.append(cursor.fetchall())
+
+        # staff data
+        cursor.execute("SELECT p.personId, s.staffId, p.username, p.firstName, p.lastName, p.sex, p.dateOfBirth, s.position, b.branchName, sr.serviceName FROM person AS p INNER JOIN staffs AS s ON s.personId = p.personId INNER JOIN branch AS b ON b.branchId = s.branchId INNER JOIN services AS sr ON sr.serviceId = s.serviceId;")
+        results.append(cursor.fetchall())
+
+        # branch table
+        cursor.execute("SELECT * FROM branch;")
+        results.append(cursor.fetchall())
+
+        # services table
+        cursor.execute("SELECT * FROM services;")
+        results.append(cursor.fetchall())
+
+        # appointments data
+        cursor.execute("SELECT a.appointmentId, a.date, a.startTime, a.endTime, a.staffId, a.custId, sr.serviceName, a.progress FROM appointments as a INNER JOIN services AS sr ON sr.serviceId = a.serviceId;")
+        results.append(cursor.fetchall())
+
+        # salary data
+        cursor.execute("SELECT sa.salaryId, sa.date, sa.amount, p.personId, s.staffId, p.username, p.firstName, p.lastName, p.sex, p.dateOfBirth, s.position, b.branchName, sr.serviceName FROM salary AS sa INNER JOIN staffs AS s ON s.staffId = sa.staffId INNER JOIN person AS p ON p.personId = s.personId INNER JOIN branch AS b  ON b.branchId = s.branchId INNER JOIN services AS sr ON sr.serviceId = s.serviceId;")
+        results.append(cursor.fetchall())
+    except get_error() as error:
+        print(error)
+        pass
+
+    return results
--- src/views.py
+++ src/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify
+import src.backend.api.tableapi as tableapi
 import requests as r
 from src.state import state
 
@@ -8,3 +9,41 @@ views = Blueprint("views", __name__)
 @views.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html", state=state)
+
+
+# Admin routes
+@views.route("/admin/all_tables", methods=["GET"])
+def all_tables():
+    if state["logged_in"]:
+        result = tableapi.get_all_tables()
+
+        return render_template("admin/all_tables.html", state=state, tables=result)
+
+    return render_template("admin/all_tables.html", state=state)
+
+
+@views.route("/admin/free_query", methods=["GET", "POST"])
+def free_query():
+    return render_template("admin/free_query.html", state=state)
+
+
+@views.route("/admin/manage_staff", methods=["GET", "POST"])
+def manage_staff():
+    return render_template("admin/manage_staff.html", state=state)
+
+
+@views.route("/admin/view_appointments", methods=["GET"])
+def view_appointments():
+    return render_template("admin/view_appointments.html", state=state)
+
+
+# Staff routes
+@views.route("/staff/manage_appointments", methods=["GET", "POST"])
+def manage_appointments():
+    return render_template("staff/manage_appointments.html", state=state)
+
+
+# Customer routes
+@views.route("/customer/book_appointments", methods=["GET", "POST"])
+def book_appointments():
+    return render_template("customer/book_appointments.html", state=state)
